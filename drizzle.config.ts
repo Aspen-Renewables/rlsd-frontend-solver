90195883701f3b35a45518db813b0744edb0a2b8
simon <simonnfts@gmail.com>
2024-06-03 12:44:07 -0400

    fixing drizzle kit to se native vercel env variable

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "drizzle-kit";
 import * as dotenv from "dotenv";
 dotenv.config();
-const dbUrl = process.env.DATABASE_URL;
+const dbUrl = process.env.POSTGRES_URL;
 if (!dbUrl) throw new Error("DATABASE_URL is required");
 export default defineConfig({
   dialect: "postgresql", // "mysql" | "sqlite" | "postgresql"
