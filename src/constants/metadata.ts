1faf10b0241a94f7ad48dd26cdb1c54b78da1959
simon <simonnfts@gmail.com>
2024-06-05 19:41:33 -0400

    changing logo

--- src/constants/metadata.ts
+++ src/constants/metadata.ts
@@ -1,3 +1,3 @@
 export const metadata = {
-  logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Acme_Markets_lolo.svg/1200px-Acme_Markets_lolo.svg.png",
+  logo: "https://static.wixstatic.com/media/9697bc_c77e036cbd0e4636bc0a9d02283aa2b4~mv2.png/v1/fill/w_762,h_238,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/aspen-renewables-logo.png",
 };
