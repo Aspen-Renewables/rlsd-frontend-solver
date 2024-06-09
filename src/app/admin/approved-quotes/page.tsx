1c8a86cc1e4c4c6684ae3067a94f8b9346211782
simon <simonnfts@gmail.com>
2024-06-09 14:57:54 -0400

    seeing if can remoe dynamic cahing on approved quotes page

--- src/app/admin/approved-quotes/page.tsx
+++ src/app/admin/approved-quotes/page.tsx
@@ -5,10 +5,12 @@ import {
 } from "@/db/queries";
 import View from "./view";
 import { db } from "@/db/db";
+export const dynamic = "force-dynamic";
 export const revalidate = 1;
 const Page = async () => {
   const approvedQuoteGroups: ReturnTypeOfGetApprovedQuoteGroups =
     await getApprovedQuoteGroups();
+
   const totalApproved = await db.query.TotalBudgetGranted.findFirst({});
   return (
     <View
